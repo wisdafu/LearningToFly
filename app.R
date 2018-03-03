8216e00aca352d51314c4deeda0070267659bac3
Chris Janowski <cjanow3@uic.edu>
2018-03-03 13:38:30 -0600

    Totals for each airline from midway/ohare in terms of arrivals and departures.

--- app.R
+++ app.R
@@ -9,6 +9,7 @@
 #
 # Import libraries
 #
+
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
@@ -22,7 +23,7 @@ library(plotly)
 # For some weird reason, there exists a column called 'X' 
 # with no data fields... So remove it via NULL
 #
-# Verify that flight date is type data and not string
+# Verify that flight date is type date and not string
 #
 
 januaryFlightData <- read.table(file = "january.csv", sep = ",", header = TRUE)
@@ -42,10 +43,40 @@ midwayOrigin <- subset(januaryFlightData, januaryFlightData$ORIGIN_AIRPORT_ID ==
 ohareDest <- subset(januaryFlightData, januaryFlightData$DEST_AIRPORT_ID == "13930")
 ohareOrigin <- subset(januaryFlightData, januaryFlightData$ORIGIN_AIRPORT_ID == "13930")
 
-# Get each day of the week
-#e.g.
-weekdays(as.Date('2017-01-01','%Y-%m-%d'))
+#
+# Get list and counts of all unique airlines in midway
+# and o'hare, then do some column reformatting
+#
+# Arrival = Dest, Departure = Origin
+#
+ohareAirlines <- as.data.frame(table(ohareDest$CARRIER))
+colnames(ohareAirlines)[colnames(ohareAirlines) == "Var1"] <- "Carrier"
+colnames(ohareAirlines)[colnames(ohareAirlines) == "Freq"] <- "Arrivals"
+ohareAirlines$Departures <- table(ohareOrigin$CARRIER)
 
+midwayAirlines <- as.data.frame(table(midwayDest$CARRIER))
+colnames(midwayAirlines)[colnames(midwayAirlines) == "Var1"] <- "Carrier"
+colnames(midwayAirlines)[colnames(midwayAirlines) == "Freq"] <- "Arrivals"
+midwayAirlines$Departures <- table(midwayOrigin$CARRIER)
+
+
+# Get total arrivals and departures for each airport
+midwayDestTotal <- nrow(midwayDest)
+midwayOriginTotal <- nrow(midwayOrigin)
+
+ohareDestTotal <- nrow(ohareDest)
+ohareOriginTotal <- nrow(ohareOrigin)
+
+# TODO: Get each day of the week
+weekdays(as.Date('2017-01-01','%Y-%m-%d'))
+ohareDestMonday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Monday")
+ohareDestTuesday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Tuesday")
+ohareDestWednesday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Wednesday")
+ohareDestThursday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Thursday")
+ohareDestFriday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Friday")
+ohareDestSaturday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Saturday")
+ohareDestSunday <- subset(ohareDest, weekdays(as.Date(ohareDest$FL_DATE,"%Y-%m-%d")) == "Sunday")
+  
 #
 # Shiny Dashboard
 #
